191f45c0660625daa3cfe0a04bb1a7d971930815
kreynolds2 <rkreynolds2@gmail.com>
2023-03-19 09:27:27 -0400

    woohoo

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,16 +1,37 @@
 import random
 
-player1 = input("Rock, Paper, or Scissors? ").lower()
-player2 = random.choice(["Rock", "Paper", "Scissors"]).lower()
-print("Janken!: ", player2)
+def rps_round():
+    player1 = input("Rock, Paper, or Scissors? ").lower()
+    player2 = random.choice(["Rock", "Paper", "Scissors"]).lower()
+    print("Janken!: ", player2)
+    winner = determine_winner(player1, player2)
+    if winner == "You tied":
+        print("TIE, AGAIN!")
+        rps_round()
+    else:
+        print(f'Winner is {winner}')
+        play_again()
 
-if player1 == "rock" and player2 == "scissors":
-    print("You Won!")
-elif player1 == "paper" and player2 == "rock":
-    print("You Won!")
-elif player1 == "scissors" and player2 == "paper":
-    print("You Won!")
-elif player1 == player2:
-    print("You tied")
-else:
-    print("pathetic")
+def determine_winner(player1_move, player2_move):
+    if (player1_move == "rock" and player2_move == "scissors") or \
+        (player1_move == "paper" and player2_move == "rock") or \
+        (player1_move == "scissors" and player2_move == "paper"):
+        return "Player 1"
+    elif player1_move == player2_move:
+        return "You tied"
+    elif (player2_move == "rock" and player1_move == "scissors") or \
+        (player2_move == "paper" and player1_move == "rock") or \
+        (player2_move == "scissors" and player1_move == "paper"):
+        return "Player 2"
+    else: 
+        print("Please input rock paper or scissors")
+        rps_round()
+
+def play_again():
+    play_again_input = input("Play Again? press Y or N ").lower()
+    if play_again_input == ("y"):
+        rps_round()
+    else:
+        print("RIP")
+
+rps_round()
